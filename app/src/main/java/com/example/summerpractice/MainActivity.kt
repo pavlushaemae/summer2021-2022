c365feca8fd90cecc6063f86b690a806710a1c81
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-06-30 04:30:47 +0300

    task1

--- app/src/main/java/com/example/summerpractice/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     private var height: EditText? = null
     private var age: EditText? = null
     private var name: EditText? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appeal = findViewById(R.id.tv_please)
@@ -26,24 +26,31 @@ class MainActivity : AppCompatActivity() {
         age = findViewById(R.id.et_age)
         name = findViewById(R.id.et_name)
         result = findViewById(R.id.te_result)
-        button?.setOnClickListener {
-            val weightNumber = weight?.text.toString().toFloat()
-            val heightNumber = height?.text.toString().toInt()
-            val ageNumber = age?.text.toString().toInt()
-            val lengthOfName = name?.text.toString().count()
-            if (ageNumber in 1..149 &&
-                lengthOfName in 1..50 &&
-                heightNumber in 1..249 &&
-                weightNumber > 0 &&
-                weightNumber < 250
-            ) {
-                val value = (weightNumber + heightNumber + ageNumber + lengthOfName)*1000
-                result?.setText("Вы любите кошек на: $value%. Поздравляю!")
-                Log.e("MainActivity","Вы любите кошек на: $value%")
-            } else {
-                result?.setText("Данные введены некорректно.")
-                Log.e("MainActivity","Данные введены некорректно.")
+            button?.setOnClickListener {
+                try{
+                        val weightNumber = weight?.text.toString().toFloat()
+                        val heightNumber = height?.text.toString().toInt()
+                        val ageNumber = age?.text.toString().toInt()
+                        val lengthOfName = name?.text.toString().count()
+
+                    if (ageNumber in 1..149 &&
+                        lengthOfName in 1..50 &&
+                        heightNumber in 1..249 &&
+                        weightNumber > 0 &&
+                        weightNumber < 250
+                    ) {
+                        val value = (weightNumber + heightNumber + ageNumber + lengthOfName)*1000
+                        result?.text = "Вы любите кошек на: $value%. Поздравляю!"
+                        Log.e("MainActivity","Вы любите кошек на: $value%")
+                    } else {
+                        result?.text = "Данные введены некорректно."
+                        Log.e("MainActivity","Данные введены некорректно.")
+                    }
+                }catch (e: Exception){
+                    result?.text = "Данные введены некорректно."
+                    Log.e("MainActivity","Данные введены некорректно.")
+                }
             }
-        }
+
     }
 }
